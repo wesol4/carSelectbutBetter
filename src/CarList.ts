7f9940294680c765896b804c8f5643d707226d78
wesol4 <wesol4@gmail.com>
2025-05-13 23:37:30 +0200

    poprawka ,

--- src/CarList.ts
+++ src/CarList.ts
@@ -22,8 +22,8 @@ cars = [
 		comments: "Mega dynamicznem, nowoczesne",
 		otomotoUrl: "https://www.otomoto.pl/osobowe/ford/puma/od-2024?search%5Bfilter_enum_gearbox%5D=automatic&search%5Bfilter_float_mileage%3Ato%5D=100&search%5Border%5D=filter_float_price%3Aasc&search%5Badvanced_search_expanded%5D=true",
 		wwwExample: "https://www.google.com/search?sca_esv=5bb96e0579041c84&sxsrf=ADLYWIIZquffq54TGTqC3tPClXx5Ev8mNw:1729939588276&q=ford+puma+2024&udm=2&fbs=AEQNm0Aa4sjWe7Rqy32pFwRj0UkWd8nbOJfsBGGB5IQQO6L3J_86uWOeqwdnV0yaSF-x2jon2iao6KWCaVjfn7ahz_sf_uPKlBgHiXUTxuTOrBgkEHAZKBArNMIg_JaUOYCTVNreNhAlJbFKEmzPuAQkoiBtIhrPYKQR3WHGzgZYwxf2NkHIa-s&sa=X&ved=2ahUKEwiilJGV76uJAxUOGhAIHbmjJQEQtKgLegQIFBAB&biw=1800&bih=945&dpr=2#vhid=aOpQEABZl-iuSM&vssid=mosaic",
-		imageUrls: ["/src/assets/puma.jpeg", "src/assets/puma2.jpeg", "src/assets/puma3.jpeg", "src/assets/pumaInt.jpeg", "src/assets/pumaInt2.jpeg"]
-		wymiary: ""
+		imageUrls: ["/src/assets/puma.jpeg", "src/assets/puma2.jpeg", "src/assets/puma3.jpeg", "src/assets/pumaInt.jpeg", "src/assets/pumaInt2.jpeg"],
+		wymiary: "4186 × 1805 × 1550 mm"
 	},
 	{
 		name: "Renault Capture",
@@ -33,7 +33,7 @@ cars = [
 		comments: "idealna wielkość, super nowocześnie wygląda, ma hybrydę, brak na razie promocji na koniec roku",
 		otomotoUrl: "https://www.otomoto.pl/osobowe/renault/captur/od-2024?search%5Bfilter_enum_country_origin%5D=pl&search%5Bfilter_enum_damaged%5D=0&search%5Bfilter_enum_fuel_type%5D%5B0%5D=petrol&search%5Bfilter_enum_fuel_type%5D%5B1%5D=petrol-cng&search%5Bfilter_enum_fuel_type%5D%5B2%5D=petrol-lpg&search%5Bfilter_enum_fuel_type%5D%5B3%5D=diesel&search%5Bfilter_enum_fuel_type%5D%5B4%5D=etanol&search%5Bfilter_enum_fuel_type%5D%5B5%5D=hybrid&search%5Bfilter_enum_fuel_type%5D%5B6%5D=plugin-hybrid&search%5Bfilter_enum_fuel_type%5D%5B7%5D=hidrogen&search%5Bfilter_enum_gearbox%5D=automatic&search%5Bfilter_float_mileage%3Ato%5D=100&search%5Border%5D=filter_float_price%3Aasc&search%5Badvanced_search_expanded%5D=true",
 		wwwExample: "https://images.app.goo.gl/32oMny2CFxVru9ta6",
-		imageUrls: ["/src/assets/capture.jpeg", "src/assets/captureInt2.jpeg"]
+		imageUrls: ["/src/assets/capture.jpeg", "src/assets/captureInt2.jpeg"],
 		wymiary: "4239 × 1797 × 1575 mm"
 	},
 	{
@@ -44,7 +44,7 @@ cars = [
 		comments: "Hybryda od 117,000",
 		otomotoUrl: "https://www.otomoto.pl/osobowe/hyundai/kona?search%5Bfilter_enum_country_origin%5D=pl&search%5Bfilter_enum_damaged%5D=0&search%5Bfilter_enum_fuel_type%5D%5B0%5D=petrol&search%5Bfilter_enum_fuel_type%5D%5B1%5D=petrol-cng&search%5Bfilter_enum_fuel_type%5D%5B2%5D=petrol-lpg&search%5Bfilter_enum_fuel_type%5D%5B3%5D=diesel&search%5Bfilter_enum_fuel_type%5D%5B4%5D=etanol&search%5Bfilter_enum_fuel_type%5D%5B5%5D=hybrid&search%5Bfilter_enum_fuel_type%5D%5B6%5D=plugin-hybrid&search%5Bfilter_enum_fuel_type%5D%5B7%5D=hidrogen&search%5Bfilter_enum_gearbox%5D=automatic&search%5Bfilter_float_mileage%3Ato%5D=100&search%5Border%5D=filter_float_price%3Aasc&search%5Badvanced_search_expanded%5D=true",
 		wwwExample: "https://images.app.goo.gl/cx6CwWHHVMep2wxA6",
-		imageUrls: ["/src/assets/kona.jpeg", "src/assets/konaInt2.jpeg", "src/assets/konaBack.jpeg" ]
+		imageUrls: ["/src/assets/kona.jpeg", "src/assets/konaInt2.jpeg", "src/assets/konaBack.jpeg" ],
 		wymiary: "4355 × 1825 × 1575 mm"
 	},
 	{
@@ -55,7 +55,7 @@ cars = [
 		comments: "5 lat gwarancji, mild hibrid",
 		otomotoUrl: "https://www.otomoto.pl/osobowe/mitsubishi/asx/od-2024?search%5Bfilter_enum_country_origin%5D=pl&search%5Bfilter_enum_damaged%5D=0&search%5Bfilter_enum_fuel_type%5D%5B0%5D=petrol&search%5Bfilter_enum_fuel_type%5D%5B1%5D=petrol-cng&search%5Bfilter_enum_fuel_type%5D%5B2%5D=petrol-lpg&search%5Bfilter_enum_fuel_type%5D%5B3%5D=diesel&search%5Bfilter_enum_fuel_type%5D%5B4%5D=etanol&search%5Bfilter_enum_fuel_type%5D%5B5%5D=hybrid&search%5Bfilter_enum_fuel_type%5D%5B6%5D=plugin-hybrid&search%5Bfilter_enum_fuel_type%5D%5B7%5D=hidrogen&search%5Bfilter_enum_gearbox%5D=automatic&search%5Border%5D=filter_float_price%3Aasc&search%5Badvanced_search_expanded%5D=true",
 		wwwExample: "https://www.google.com/search?sca_esv=31fc9eb8bb598b81&sxsrf=ADLYWIJtQsf5sgfEcOAjn2_0D0u1q1YQuQ:1729592537749&q=Mitsubishi+ASX+2024&udm=2&fbs=AEQNm0Aa4sjWe7Rqy32pFwRj0UkWd8nbOJfsBGGB5IQQO6L3J_86uWOeqwdnV0yaSF-x2jon2iao6KWCaVjfn7ahz_sf_uPKlBgHiXUTxuTOrBgkEHAZKBArNMIg_JaUOYCTVNreNhAlJbFKEmzPuAQkoiBtIhrPYKQR3WHGzgZYwxf2NkHIa-s&sa=X&ved=2ahUKEwiy-cWm4qGJAxUXgv0HHeN_FvMQtKgLegQIHRAB&biw=1909&bih=1925&dpr=1",
-		imageUrls: ["/src/assets/asx.jpeg", "src/assets/asxInt.jpeg"]
+		imageUrls: ["/src/assets/asx.jpeg", "src/assets/asxInt.jpeg"],
 		wymiary: "4227 × 1797 × 1567 mm"
 	},
 	{
@@ -66,7 +66,7 @@ cars = [
 		comments: "Jakościowe, klimatyzacja automatyczna (dwustrefowa), 17 felgi, slabe klamki",
 		otomotoUrl: "https://www.otomoto.pl/osobowe/toyota/c-hr?search%5Bfilter_enum_country_origin%5D=pl&search%5Bfilter_enum_damaged%5D=0&search%5Bfilter_enum_fuel_type%5D%5B0%5D=petrol&search%5Bfilter_enum_fuel_type%5D%5B1%5D=petrol-cng&search%5Bfilter_enum_fuel_type%5D%5B2%5D=petrol-lpg&search%5Bfilter_enum_fuel_type%5D%5B3%5D=diesel&search%5Bfilter_enum_fuel_type%5D%5B4%5D=etanol&search%5Bfilter_enum_fuel_type%5D%5B5%5D=hybrid&search%5Bfilter_enum_fuel_type%5D%5B6%5D=plugin-hybrid&search%5Bfilter_enum_fuel_type%5D%5B7%5D=hidrogen&search%5Bfilter_enum_gearbox%5D=automatic&search%5Bfilter_float_mileage%3Ato%5D=100&search%5Border%5D=filter_float_price%3Aasc&search%5Badvanced_search_expanded%5D=true",
 		wwwExample: "https://images.app.goo.gl/t9sbBjjfVKXTdoVr5",
-		imageUrls: ["/src/assets/chr.jpg", "/src/assets/chrInt2.jpeg"]
+		imageUrls: ["/src/assets/chr.jpg", "/src/assets/chrInt2.jpeg"],
 		wymiary: "4360 × 1830 × 1570 mm"
 	},
 	{
@@ -77,7 +77,7 @@ cars = [
 		comments: "Duży, masywny, bezpieczny, hybryda",
 		otomotoUrl: "https://www.otomoto.pl/osobowe/dacia/duster?search%5Bfilter_enum_country_origin%5D=pl&search%5Bfilter_enum_damaged%5D=0&search%5Bfilter_enum_fuel_type%5D%5B0%5D=petrol&search%5Bfilter_enum_fuel_type%5D%5B1%5D=petrol-cng&search%5Bfilter_enum_fuel_type%5D%5B2%5D=petrol-lpg&search%5Bfilter_enum_fuel_type%5D%5B3%5D=diesel&search%5Bfilter_enum_fuel_type%5D%5B4%5D=etanol&search%5Bfilter_enum_fuel_type%5D%5B5%5D=hybrid&search%5Bfilter_enum_fuel_type%5D%5B6%5D=plugin-hybrid&search%5Bfilter_enum_fuel_type%5D%5B7%5D=hidrogen&search%5Bfilter_enum_gearbox%5D=automatic&search%5Bfilter_enum_generation%5D=gen-iii-2024&search%5Bfilter_float_mileage%3Ato%5D=100&search%5Border%5D=filter_float_price%3Aasc&search%5Badvanced_search_expanded%5D=true",
 		wwwExample: "https://images.app.goo.gl/NoabLVEm31uSr9AVA",
-		imageUrls: ["/src/assets/duster.jpeg", "src/assets/dasterInt.jpeg"]
+		imageUrls: ["/src/assets/duster.jpeg", "src/assets/dasterInt.jpeg"],
 		wymiary: "4344 × 1813 × 1656 mm"
 	},
 	{
@@ -88,7 +88,7 @@ cars = [
 		comments: "Tanio, ale chińśczyk",
 		otomotoUrl: "https://www.otomoto.pl/osobowe/mg/hs?search%5Bfilter_enum_country_origin%5D=pl&search%5Bfilter_enum_damaged%5D=0&search%5Bfilter_enum_fuel_type%5D%5B0%5D=petrol&search%5Bfilter_enum_fuel_type%5D%5B1%5D=petrol-cng&search%5Bfilter_enum_fuel_type%5D%5B2%5D=petrol-lpg&search%5Bfilter_enum_fuel_type%5D%5B3%5D=diesel&search%5Bfilter_enum_fuel_type%5D%5B4%5D=etanol&search%5Bfilter_enum_fuel_type%5D%5B5%5D=hybrid&search%5Bfilter_enum_fuel_type%5D%5B6%5D=plugin-hybrid&search%5Bfilter_enum_fuel_type%5D%5B7%5D=hidrogen&search%5Bfilter_enum_gearbox%5D=automatic&search%5Bfilter_float_mileage%3Ato%5D=100&search%5Border%5D=filter_float_price%3Aasc&search%5Badvanced_search_expanded%5D=true",
 		wwwExample: "",
-		imageUrls: ["/src/assets/mghs_2.jpg", "/src/assets/mghs_1.webp"]
+		imageUrls: ["/src/assets/mghs_2.jpg", "/src/assets/mghs_1.webp"],
 		wymiary: "4670 × 1890 × 1663 mm"
 	},
 	{
@@ -99,7 +99,7 @@ cars = [
 		comments: "Mega fajnie w środku, dobry feeling",
 		otomotoUrl: "https://www.otomoto.pl/osobowe/nissan/qashqai?search%5Bfilter_enum_country_origin%5D=pl&search%5Bfilter_enum_damaged%5D=0&search%5Bfilter_enum_fuel_type%5D%5B0%5D=petrol&search%5Bfilter_enum_fuel_type%5D%5B1%5D=petrol-cng&search%5Bfilter_enum_fuel_type%5D%5B2%5D=petrol-lpg&search%5Bfilter_enum_fuel_type%5D%5B3%5D=diesel&search%5Bfilter_enum_fuel_type%5D%5B4%5D=etanol&search%5Bfilter_enum_fuel_type%5D%5B5%5D=hybrid&search%5Bfilter_enum_fuel_type%5D%5B6%5D=plugin-hybrid&search%5Bfilter_enum_fuel_type%5D%5B7%5D=hidrogen&search%5Bfilter_enum_gearbox%5D=automatic&search%5Bfilter_enum_generation%5D=gen-iii-2021&search%5Bfilter_float_mileage%3Ato%5D=100&search%5Border%5D=filter_float_price%3Aasc&search%5Badvanced_search_expanded%5D=true",
 		wwwExample: "https://images.app.goo.gl/Gotkx6i3az5LyugEA",
-		imageUrls: ["/src/assets/qashqai.jpeg", "src/assets/qashqaiInt.jpeg", "src/assets/qashqaiBack.jpeg"]
+		imageUrls: ["/src/assets/qashqai.jpeg", "src/assets/qashqaiInt.jpeg", "src/assets/qashqaiBack.jpeg"],
 		wymiary: "4425 × 1838 × 1635 mm"
 	},
 	{
@@ -110,7 +110,7 @@ cars = [
 		comments: "Będzie w listopadzie, 7 lat gwarancji, chińczyk",
 		otomotoUrl: "https://www.otomoto.pl/osobowe/mg/zs?search%5Bfilter_enum_country_origin%5D=pl&search%5Bfilter_enum_damaged%5D=0&search%5Bfilter_enum_fuel_type%5D%5B0%5D=petrol&search%5Bfilter_enum_fuel_type%5D%5B1%5D=petrol-cng&search%5Bfilter_enum_fuel_type%5D%5B2%5D=petrol-lpg&search%5Bfilter_enum_fuel_type%5D%5B3%5D=diesel&search%5Bfilter_enum_fuel_type%5D%5B4%5D=etanol&search%5Bfilter_enum_fuel_type%5D%5B5%5D=hybrid&search%5Bfilter_enum_fuel_type%5D%5B6%5D=plugin-hybrid&search%5Bfilter_enum_fuel_type%5D%5B7%5D=hidrogen&search%5Bfilter_float_mileage%3Ato%5D=100&search%5Border%5D=filter_float_price%3Aasc&search%5Badvanced_search_expanded%5D=true",
 		wwwExample: "https://www.youtube.com/watch?app=desktop&v=z_kZuv5GnN8",
-		imageUrls: ["/src/assets/zs.jpeg", "src/assets/zsInt.jpeg"]
+		imageUrls: ["/src/assets/zs.jpeg", "src/assets/zsInt.jpeg"],
 		wymiary: "4430 × 1818 × 1635 mm "
 	},
 ];
