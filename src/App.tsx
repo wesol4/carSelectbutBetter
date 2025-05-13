803c1306143a31b505ed76b08dca3fe092315c7f
wesol4 <wesol4@gmail.com>
2025-05-13 18:55:16 +0200

    Zaczynamy nawa wersje, usunalem kilka samochodw + dodalem mg hs, zdjecia w asset zostaly wiewc trzeba bedzie sie ich pozbyc

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import CarGallery from './CarGallery';
 const App: React.FC = () => {
     return (
         <div>
-            <h1>WYBIERZ AUTO</h1>
+            <h1>CHOOSE your dream car</h1>
             <CarGallery />
         </div>
     );
